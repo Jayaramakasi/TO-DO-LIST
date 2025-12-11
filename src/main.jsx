5ba2b31d73e4c4f497a9c61131719c2e067e3c43
KASI JAYA RAMARAO <20255-cm-025@aec.edu.in>
2025-12-11 14:36:57 +0530

    Wrap App component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+//import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename-"/TO-DO-LIST">
+   <App />
+  </BrowserRouter>
   
-    <App />
+   
   
 )
